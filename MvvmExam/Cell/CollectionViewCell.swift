1cd4afb0b32e8f0bc684a78a6123c9b3dec61843
KimMinKu <passion.kmk@gmail.com>
2018-09-06 16:27:57 +0900

    Create Cell file

---
+++ MvvmExam/Cell/CollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CollectionViewCell.swift
+//  MvvmExam
+//
+//  Created by MinKu Kim on 2018. 9. 6..
+//  Copyright © 2018년 KimMinKu. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Overrides
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subTitleLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}

8177a684f070b56c1de4a890d1562598c23d0538
passionkmk <passion.kmk@gmail.com>
2018-12-10 01:29:51 +0900

    Create AppDataModel

---
+++ MvvmExam/Model/AppDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  AppDataModel.swift
+//  MvvmExam
+//
+//  Created by 김민구 on 10/12/2018.
+//  Copyright © 2018 KimMinKu. All rights reserved.
+//
+
+import Foundation
+
+class AppDataModel {
+    var datas: [AppData] = []
+}
